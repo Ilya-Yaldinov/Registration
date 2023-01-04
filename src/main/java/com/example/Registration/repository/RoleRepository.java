2725486b6456e55b213e4ca5174776d3bc1226dc
by_Onyx <ilyayaldinov@gmail.com>
2023-01-04 22:00:11 +0500

    Initial commit

---
+++ src/main/java/com/example/Registration/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.Registration.repository;
+
+import com.example.Registration.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
